58d010d67edbb68e250541cfcc32b222e6811581
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-12 04:34:14 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,21 +1,30 @@
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   author_id INTEGER,
-  sub_genre_id INTEGER
+  subgenre_id INTEGER
 );
-CREATE TABLE sub_genres (
+CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
   name TEXT
 );
-CREATE TABLE  (
+CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
-  
+  name TEXT
 );
-CREATE TABLE (
+CREATE TABLE books (
   id INTEGER PRIMARY KEY,
-  
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
 );
-CREATE TABLE (
+CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
-  
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER
+);
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY
 );
